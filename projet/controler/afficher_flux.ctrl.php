04ba139e491f504f36dea6045df70273ebd39bef
jorane <joraneju@gmail.com>
2015-11-04 19:58:43 +0100

    test"

--- projet/controler/afficher_flux.ctrl.php
+++ projet/controler/afficher_flux.ctrl.php
@@ -1,7 +1,19 @@
 <?php
-
 include_once("../model/DAO.class.php");
-
+	
+	$q = "SELECT url FROM RSS";
+	$r = $dao->getDB()->query($q);
+	$result = $r->fetchAll();
+	//var_dump($result);
+	$i=0;
+	foreach($result as $value)
+	{
+		foreach($value as $val)
+		{
+			$table[$i]=$val;
+			$i++;
+		}
+	}
+	
 require_once("../view/afficher_flux.view.php");
-
 ?>
